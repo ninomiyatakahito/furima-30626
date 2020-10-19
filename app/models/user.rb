d680a3f755e0b998077bcc223046f2ce556d222f
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-19 18:06:55 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,              presence: true
-         validates :first_name,            presence: true
-         validates :last_name,             presence: true
-         validates :first_name_kana,       presence: true
-         validates :last_name_kana,        presence: true
-         validates :birthday,              presence: true
+         validates :email,                 format: { with: /@/}
+         validates :password,         length: { minimum: 6 }, confirmation: true,  format: { with: /\A[a-zA-Z0-9]+\z/ }
+      with_options presence: true do
+         validates :nickname
+         validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :last_name,        format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+         validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :last_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :birthday
+      end
 
   has_many :items, dependent: :destroy
   has_many :purchases, dependent: :destroy
 end
-
