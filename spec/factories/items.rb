35821cf20db6cd0545e798d1b532dde0867e4df0
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-24 14:37:24 +0900

    商品出品機能修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,12 +3,12 @@ FactoryBot.define do
 
     name                  {"ペン"}
     description           {"クロペンです"}
-    category_id           {"1"}
-    condition_id          {"1"}
-    shopping_cost_id      {"1"}
-    prefecture_id         {"1"}
-    shopping_days_id      {"1"}
-    price                 {"1000"}
+    category_id           {1}
+    condition_id          {1}
+    shopping_cost_id      {1}
+    prefecture_id         {1}
+    shopping_days_id      {1}
+    price                 {1000}
     association :user             
 
     after(:build) do |item|
