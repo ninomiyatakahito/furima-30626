d535d12aa027b4191b14442cf698da12399feaf5
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-24 17:24:39 +0900

    修正箇所

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @item = Item.order("created_at DESC")
     @items = Item.all.order("created_at DESC")
   end
 
