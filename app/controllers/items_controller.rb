bf1548bd11f1952c690dc129df2c68e9b1fe34cd
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-24 13:12:52 +0900

    商品出品機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.valit?
+    @item.valid?
     if @item.save
       redirect_to root_path
     else
