287c388f27dfe8b15a3865b90603f41741a82732
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-25 01:54:48 +0900

    登録した情報

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,7 +22,8 @@ class ItemsController < ApplicationController
     end
   end
 
-  def item_params
+  private
+  def set_item
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :prefecture_id, :shopping_cost_id, :shopping_days_id, :price).merge(user_id: current_user.id)
   end
 end
