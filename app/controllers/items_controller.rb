71480c6298e0bda2990456517271f2f327e2b9e0
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-25 17:04:37 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,6 @@ class ItemsController < ApplicationController
     end
   end
 
-
   def show
   end
 
@@ -42,6 +41,5 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
-
   end
 end
