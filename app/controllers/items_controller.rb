1ccae17b12ba6ebd7aa1d8140d77b0314dc8432e
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-25 15:59:25 +0900

    アクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -18,7 +19,21 @@ class ItemsController < ApplicationController
     end
   end
 
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :prefecture_id, :shopping_cost_id, :shopping_days_id, :price).merge(user_id: current_user.id)
+
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+
   end
 end
