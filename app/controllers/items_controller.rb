02d188a30f8766458cd306eaedc319f43ad72cde
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-25 21:49:43 +0900

    修正箇所

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, except: [:index, :show, ]
+  before_action :forbid_login_user, only: :edit
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -24,16 +25,20 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def edit
-    end
+  def edit 
   end
 
   def update
+      if @item.update(item_params)
+         redirect_to item_path
+      else
+        render :edit
+      end
   end
-  
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :prefecture_id, :shopping_cost_id, :shopping_days_id, :price).merge(user_id: current_user.id)
+
   end
 
   def set_item
@@ -46,8 +51,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  def authenticate_user
-    if Item.find(params[:id]).seller != current_user
-      redirect_to root_path
+  def forbid_login_user
+    unless current_user.id == @item.user.id
+      redirect_to item_path
     end
+  end
 end
