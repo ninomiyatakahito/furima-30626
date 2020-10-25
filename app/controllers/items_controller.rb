122a59410eb52cc7403ea2bb88fd8eec8adc35cf
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-25 21:58:13 +0900

    修正箇所

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,10 +35,11 @@ class ItemsController < ApplicationController
         render :edit
       end
   end
+
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :prefecture_id, :shopping_cost_id, :shopping_days_id, :price).merge(user_id: current_user.id)
-
   end
 
   def set_item
