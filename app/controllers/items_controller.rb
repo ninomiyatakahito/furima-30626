37a8547851d409eb2af8c17134d074f234d28f86
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-26 11:06:37 +0900

    修正箇所

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show, ]
   before_action :forbid_login_user, only: [:edit, :destroy]
