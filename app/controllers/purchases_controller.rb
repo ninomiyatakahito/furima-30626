ad4839985bebe733538104d5ec6e3c71e7cabf66
ninomiyatakahito <ghexcffb@gmail.com>
2020-10-27 21:00:44 +0900

    コントローラー記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,21 +1,23 @@
 class PurchasesController < ApplicationController
-  
+  before_action :set_item, only: [:index, :create]
 
   def index
     @purchase_address = PurchaseAddress.new
-    @item = Item.find(params[:item_id])
   end
   
   def create
-    @purchase_street_address = PurchasestreetAddress.new(purchase_params)
-    @purchase_street_address.save
-    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    @purchase_address.save
   end
 
   private
 
   def purchase_params
-    params.require(:purchasestreet_address).permit(:post_code, :prefecture_id, :city, :address, :appointment_name, :phone_number, :purchase).merge(user_id: current_user.id, token: params[:token], item_id: params[:item_id])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :street, :building_name, :phone_number, :purchase).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
