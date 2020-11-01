180ee530e551ab2e40cfdcf795674ff503d83d94
ninomiyatakahito <ghexcffb@gmail.com>
2020-11-01 16:56:38 +0900

    修正箇所

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_f6579d3e748826ed40ada43e");
+  Payjp.setPublicKey((process.env.PAYJP_PUBLIC_KEY));
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
